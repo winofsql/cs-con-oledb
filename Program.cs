7bccc01f81ecd35f9f1afbf547c3a6a24e8b6cc2
sworc <lightbox@nifty.com>
2021-10-31 21:04:36 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace cs_con_oledb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OleDbConnection myConAccess;
+
+            // *************************************
+            // System.Data.OleDb
+            // *************************************
+            myConAccess = new OleDbConnection();
+            myConAccess.ConnectionString =
+                string
+                    .Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};",
+                    @"\app\workspace\subject-1031\cs-con-oledb\販売管理.accdb");
+
+            // 接続を開く
+            try
+            {
+                myConAccess.Open();
+
+                myConAccess.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("接続エラーです:" + ex.Message);
+            }
+        }
+    }
+}
